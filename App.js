7d1cb451a3e5956b643c75f27449f60da29f10c1
JavaidAhmadKhan <relaxjavaid@gmail.com>
2023-01-12 00:45:03 +0530

    finished the project

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import Game from "./Game";
+
+export default function App() {
+  return (
+    <GestureHandlerRootView style={styles.container}>
+      <Game />
+      <StatusBar style="auto" />
+    </GestureHandlerRootView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
